8ba91a44b3ea5f44b38e8919f876f8f303e3cdf8
Hiển Trần Thành <tranthanhhien379@hotmail.com>
2026-04-16 15:02:45 +0700

    fix layout trang admin

--- crm-promax/src/app/layout.tsx
+++ crm-promax/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import QueryProvider from "@/components/providers/QueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +30,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {children}
-        <Toaster theme="dark" />
+        <QueryProvider>
+          {children}
+          <Toaster theme="dark" />
+        </QueryProvider>
       </body>
     </html>
   );
