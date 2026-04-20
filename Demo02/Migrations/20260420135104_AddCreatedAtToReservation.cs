d1855e6851c736cdf6a9272818b23ce7427fa804
Hiển Trần Thành <tranthanhhien379@hotmail.com>
2026-04-20 20:52:58 +0700

    update migrations

---
+++ Demo02/Migrations/20260420135104_AddCreatedAtToReservation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Demo02.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedAtToReservation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
