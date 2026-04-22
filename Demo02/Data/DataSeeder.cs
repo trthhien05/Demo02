c197cd27f3733a900d696ef00fa2f29e060f8a7d
Hiển Trần Thành <tranthanhhien379@hotmail.com>
2026-04-22 20:42:46 +0700

    commit data

--- Demo02/Data/DataSeeder.cs
+++ Demo02/Data/DataSeeder.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Security.Cryptography;
 using ConnectDB.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ConnectDB.Data;
